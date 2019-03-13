cd5faf9ee9b6f51de461c035ea300a159fbba966
Matthew Tran <matthew@Matthews-MacBook-Pro.local>
2019-03-13 15:11:41 -0700

    add 6 questions

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function submitQuiz() {
     }
 
 // calc score with answerScore function
-    var calcScore = (answerScore('q1') + answerScore('q2') + answerScore('q3') + answerScore('q4'));
+    var calcScore = (answerScore('q1') + answerScore('q2') + answerScore('q3') + answerScore('q4') + answerScore('q5') + answerScore('q6') + answerScore('q7') + answerScore('q8') + answerScore('q9') + answerScore('q10'));
     console.log("CalcScore: " + calcScore); // it works!
 
 // function to return correct answer string
@@ -32,6 +32,7 @@ function submitQuiz() {
         }
 
 // print correct answers only if wrong (calls correctAnswer function)
+
     if (answerScore('q1') === 0) {
         document.getElementById('correctAnswer1').innerHTML = correctAnswer('correctString1', 1);
     }
@@ -43,6 +44,24 @@ function submitQuiz() {
     }
     if (answerScore('q4') === 0) {
         document.getElementById('correctAnswer4').innerHTML = correctAnswer('correctString4', 4);
+    }
+    if (answerScore('q5') === 0) {
+        document.getElementById('correctAnswer5').innerHTML = correctAnswer('correctString5', 5);
+    }
+    if (answerScore('q6') === 0) {
+        document.getElementById('correctAnswer6').innerHTML = correctAnswer('correctString6', 6);
+    }
+    if (answerScore('q7') === 0) {
+        document.getElementById('correctAnswer7').innerHTML = correctAnswer('correctString7', 7);
+    }
+    if (answerScore('q8') === 0) {
+        document.getElementById('correctAnswer8').innerHTML = correctAnswer('correctString8', 8);
+    }
+    if (answerScore('q9') === 0) {
+        document.getElementById('correctAnswer9').innerHTML = correctAnswer('correctString9', 9);
+    }
+    if (answerScore('q10') === 0) {
+        document.getElementById('correctAnswer10').innerHTML = correctAnswer('correctString10', 10);
     }
 
 // calculate "possible score" integer
